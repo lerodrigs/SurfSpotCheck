a0c1b341313838fad58880aa42deb65a7420fa00
lerodrigs <morango110895>
2017-07-11 00:12:08 -0300

    nav

---
+++ app/src/main/java/com/surfspotcheck/surfspotcheck/Models/NavMenuItem.java
@@ -0,0 +1,35 @@
+package com.surfspotcheck.surfspotcheck.Models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NavMenuItem
+{
+    public NavMenuItem()
+    {
+        this.position = 0;
+        this.name = "";
+    }
+
+    public int position;
+    public String name;
+
+    public List<NavMenuItem> getItems()
+    {
+        List<NavMenuItem> nav = new ArrayList<NavMenuItem>();
+        NavMenuItem item = new NavMenuItem();
+
+        item.position = 1;
+        item.name = "Home";
+        nav.add(item);
+
+        item.position = 2;
+        item.name = "Mares";
+        nav.add(item);
+
+        item.position = 3;
+        item.name = "Picos";
+
+        return nav;
+    }
+}
